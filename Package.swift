d6a6444bcf7a84eb0189f23229a331a091b95bab
consuelita <adelita@operatorfoundation.org>
2023-06-16 17:44:21 -0500

    swift-tools-version: 5.8, macOS v13

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Antiphony",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "Antiphony",
